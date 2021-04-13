897f36097fcb8c6eda4cd5a61a9b17ddb4886cf9
Oskar <oskar996@o2.pl>
2021-04-13 19:24:57 +0200

    Dodaj pliki projektów.

---
+++ Kartkowka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Kartkowka
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Surname { get; set; }
+    }
+
+    public class Poczekalnia : DbContext
+    {
+        public DbSet<Patient> Patients{ get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new Poczekalnia())
+            {
+                var s = new Patient() { Surname = "Kowalski" };
+
+                db.Patients.Add(s);
+                db.SaveChanges();
+
+                foreach(var ss in db.Patients)
+                {
+                    Console.WriteLine($"Nazwiska pacjenta: {ss.Surname}");
+                }
+            }
+        }
+    }
+}
